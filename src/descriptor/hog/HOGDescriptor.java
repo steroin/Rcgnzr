6489f51ce3a2bb35f31750ea14ad65408422a858
DESKTOP-JVGBPF7\Sergiusz <deephans@o2.pl>
2017-07-10 18:36:46 +0200

    Added hog test resources + HOG algorithm improved

--- src/descriptor/hog/HOGDescriptor.java
+++ src/descriptor/hog/HOGDescriptor.java
@@ -2,45 +2,43 @@ package descriptor.hog;
 
 import descriptor.FeatureData;
 import descriptor.FeatureDescriptor;
+
 import java.awt.image.BufferedImage;
 
 /**
  * Created by Sergiusz on 09.07.2017.
  */
 public class HOGDescriptor implements FeatureDescriptor {
-
     private HOGSettings settings;
     private final BufferedImage source;
+    private double[][] magnitudes;
+    private double[][] directions;
 
     public HOGDescriptor(BufferedImage src) {
         source = src;
     }
 
-    @Override
-    public FeatureData describe() {
-        int width = source.getWidth();
-        int height = source.getHeight();
-
-        double[][] magnitudes = new double[width][height];
-        double[][] directions = new double[width][height];
+    private void countPolarCoordinates(int width, int height) {
+        magnitudes = new double[height][width];
+        directions = new double[height][width];
 
         for (int i = 0; i < height; i++) {
             boolean hasTop = i - 1 >= 0;
             boolean hasBottom = i + 1 < height;
 
             for (int j = 0; j < width; j++) {
-                int top = ((hasTop ? source.getRGB(i - 1, j) >> 16 & 0xFF : 0) +
-                        (hasTop ? source.getRGB(i - 1, j) >> 8 & 0xFF : 0) +
-                        (hasTop ? source.getRGB(i - 1, j) & 0xFF : 0))/3;
-                int bottom = ((hasBottom ? source.getRGB(i + 1, j) >> 16 & 0xFF : 0) +
-                        (hasBottom ? source.getRGB(i + 1, j) >> 8 & 0xFF : 0) +
-                        (hasBottom ? source.getRGB(i + 1, j) & 0xFF : 0))/3;
-                int left = ((j - 1 >= 0 ? source.getRGB(i, j - 1) >> 16 & 0xFF : 0) +
-                        (j - 1 >= 0 ? source.getRGB(i, j - 1) >> 8 & 0xFF : 0) +
-                        (j - 1 >= 0 ? source.getRGB(i, j - 1) & 0xFF : 0))/3;
-                int right = ((j + 1 < width ? source.getRGB(i, j + 1) >> 16 & 0xFF : 0) +
-                        (j + 1 < width ? source.getRGB(i, j + 1) >> 8 & 0xFF : 0) +
-                        (j + 1 < width ? source.getRGB(i, j + 1) & 0xFF : 0))/3;
+                int top = ((hasTop ? source.getRGB(j, i - 1) >> 16 & 0xFF : 0) +
+                        (hasTop ? source.getRGB(j, i - 1) >> 8 & 0xFF : 0) +
+                        (hasTop ? source.getRGB(j, i - 1) & 0xFF : 0))/3;
+                int bottom = ((hasBottom ? source.getRGB(j, i + 1) >> 16 & 0xFF : 0) +
+                        (hasBottom ? source.getRGB(j, i + 1) >> 8 & 0xFF : 0) +
+                        (hasBottom ? source.getRGB(j, i + 1) & 0xFF : 0))/3;
+                int left = ((j - 1 >= 0 ? source.getRGB(j - 1, i) >> 16 & 0xFF : 0) +
+                        (j - 1 >= 0 ? source.getRGB(j - 1, i) >> 8 & 0xFF : 0) +
+                        (j - 1 >= 0 ? source.getRGB(j - 1, i) & 0xFF : 0))/3;
+                int right = ((j + 1 < width ? source.getRGB(j + 1, i) >> 16 & 0xFF : 0) +
+                        (j + 1 < width ? source.getRGB(j + 1, i) >> 8 & 0xFF : 0) +
+                        (j + 1 < width ? source.getRGB(j + 1, i) & 0xFF : 0))/3;
 
                 int hotizontalDeriv = right - left;
                 int verticalDeriv = bottom - top;
@@ -49,26 +47,24 @@ public class HOGDescriptor implements FeatureDescriptor {
                 directions[i][j] = (Math.toDegrees(Math.atan2(verticalDeriv, hotizontalDeriv)) + 180) % 180;
             }
         }
+    }
 
-        double[][][] histograms = new double[height / 8][width / 8][9];
-        for (int i = 0; i < height / 8; i++) {
-            for (int j = 0; j < width / 8; j++) {
+    private double[] getHistogram(int row, int col) {
+        double[] histogram = new double[]{0, 0, 0, 0, 0, 0, 0, 0, 0};
 
-                double[] histogram = new double[]{0, 0, 0, 0, 0, 0, 0, 0, 0};
-
-                for (int k = 0; k < 8; k++) {
-                    for (int l = 0; l < 8; l++) {
-                        int x = i + k;
-                        int y = j + l;
-                        int histogramIndex = (int) (directions[x][y] / 20);
-                        histogram[histogramIndex] = histogram[histogramIndex] + (1 - (directions[x][y] % 20) / 20) * magnitudes[x][y];
-                        histogram[(histogramIndex + 1) % histogram.length] = ((directions[x][y] % 20) / 20) * magnitudes[x][y];
-                    }
-                }
-                histograms[i][j] = histogram;
+        for (int i = 0; i < 8; i++) {
+            for (int j = 0; j < 8; j++) {
+                int x = row * 8 + i;
+                int y = col * 8 + j;
+                int histogramIndex = (int) (directions[x][y] / 20);
+                histogram[histogramIndex] = histogram[histogramIndex] + (1 - (directions[x][y] % 20) / 20) * magnitudes[x][y];
+                histogram[(histogramIndex + 1) % histogram.length] = ((directions[x][y] % 20) / 20) * magnitudes[x][y];
             }
         }
+        return histogram;
+    }
 
+    private double[] calculateFeatures(int width, int height, double[][][] histograms) {
         double[] features = new double[(height / 8 - 1) * (width / 8 - 1) * 36];
         int index = 0;
         for (int i = 0; i < height / 8 - 1; i++) {
@@ -89,6 +85,23 @@ public class HOGDescriptor implements FeatureDescriptor {
                 }
             }
         }
+        return features;
+    }
+    @Override
+    public FeatureData describe() {
+        int width = source.getWidth();
+        int height = source.getHeight();
+
+        countPolarCoordinates(width, height);
+
+        double[][][] histograms = new double[height / 8][width / 8][9];
+        for (int i = 0; i < height / 8; i++) {
+            for (int j = 0; j < width / 8; j++) {
+                histograms[i][j] = getHistogram(i, j);
+            }
+        }
+
+        double[] features = calculateFeatures(width, height, histograms);
         return new FeatureData(features);
     }
 
@@ -96,5 +109,4 @@ public class HOGDescriptor implements FeatureDescriptor {
     public BufferedImage getSource() {
         return source;
     }
-
 }
