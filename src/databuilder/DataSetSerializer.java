c2c366bb6779e82471adf7f2186e51dd94970d37
DESKTOP-JVGBPF7\Sergiusz <deephans@o2.pl>
2017-07-11 21:12:36 +0200

    Added DataSet serialization functionality

--- src/databuilder/DataSetSerializer.java
+++ src/databuilder/DataSetSerializer.java
@@ -33,7 +33,17 @@ public class DataSetSerializer {
         ArrayList<Double> classes = dataSet.getClassifications();
 
         try {
-            try (FileWriter fw = new FileWriter(destination)) {
+            try (BufferedWriter bw = new BufferedWriter(new FileWriter(destination))) {
+                for (int i = 0; i < features.size(); i++) {
+                    double[] vector = features.get(i);
+
+                    for (int j = 0; j < vector.length; j++) {
+                        bw.write(vector[j]+"");
+                        if (j < vector.length - 1) bw.write(";");
+                    }
+                    bw.write(" "+classes.get(i));
+                    bw.newLine();
+                }
             }
         } catch(IOException e) {
             errorHandler.handle(new Error("DataSetSerializer error: cannot access destination file.", ErrorType.IMPORTANT));
@@ -41,6 +51,6 @@ public class DataSetSerializer {
     }
 
     public DataSet deserialize(File source) {
-        return null;
+
     }
 }
