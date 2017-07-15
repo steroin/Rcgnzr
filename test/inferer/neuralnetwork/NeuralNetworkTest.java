133689b36d3c54110443dc724f042a0055223b99
DESKTOP-JVGBPF7\Sergiusz <deephans@o2.pl>
2017-07-15 20:33:39 +0200

    Added NeuralNetwork error calculating test

--- test/inferer/neuralnetwork/NeuralNetworkTest.java
+++ test/inferer/neuralnetwork/NeuralNetworkTest.java
@@ -38,6 +38,40 @@ public class NeuralNetworkTest {
 
     @Test
     public void calculateErrors() throws Exception {
+        NeuralNetwork nn = new NeuralNetwork(5, 2, new int[]{4, 4});
+        double[][][] body = new double[][][]{
+                {
+                        {0.4, 0.25, 0.45, 0.9, 0.1, 0.1},
+                        {0.65, 0.8, 0.5, 0.15, 0.3, 0.2},
+                        {0.7, 1, 0.35, 0.2, 0.2, 0.5},
+                        {0.1 ,0.3, 0.5, 0.7, 0.4, 0.9}
+                },
+                {
+                        {0.3, 0.1, 0.8, 0.25, 0.8},
+                        {0.8, 0.4, 0.75, 0.4, 0.6},
+                        {0.7, 0.5, 0.8, 0.9, 0.5},
+                        {0.9, 0.65, 0.8, 0.1, 0.3}
+                }
+        };
+
+        double[] input = new double[] {6, 4, 5, 8, 1};
+        double[] expectedOutput = new double[]{0.6, 0.4, 0.2, 0.6};
+        double[][] expectedErrors = new double[][]{
+                {-0.02861423693, -0.02150704204, -0.02834456121, -0.01901399856},
+                {0.0129307, -0.039424, -0.09023835, -0.08758725691}
+        };
+
+        nn.setUpWeights(body);
+        double[] output = nn.respond(input);
+        nn.calculateErrors(output, expectedOutput);
+        double[][] errors = nn.getErrors();
+
+        for (double[] layer : errors) {
+            for (double neuron : layer) {
+                System.out.println(neuron);
+            }
+            System.out.println("------------");
+        }
     }
 
 }
