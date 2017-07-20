53c3cdc7c1feca020134f4c2844ed3a3352927f0
DESKTOP-JVGBPF7\Sergiusz <deephans@o2.pl>
2017-07-20 22:16:18 +0200

    simple image data set builder GUI added

---
+++ src/data_builder/images/gui/Main.java
@@ -0,0 +1,18 @@
+package data_builder.images.gui;/**
+ * Created by Sergiusz on 20.07.2017.
+ */
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+
+    }
+}
