efe940ed7034b3a85944e624ddc612c317288b8c
DESKTOP-JVGBPF7\Sergiusz <deephans@o2.pl>
2017-07-20 21:28:05 +0200

    Image sets builder initial commit

---
+++ src/data_builder/images/gui/MainWindow.java
@@ -0,0 +1,16 @@
+package data_builder.images.gui;
+
+import javax.swing.*;
+
+/**
+ * Created by Sergiusz on 20.07.2017.
+ */
+public class MainWindow {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Image images");
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        frame.setSize(800, 600);
+        frame.setVisible(true);
+    }
+
+}
---
+++ src/data_builder/images/logic/ImageManager.java
@@ -0,0 +1,14 @@
+package data_builder.images.logic;
+
+import java.awt.image.BufferedImage;
+import java.io.File;
+
+/**
+ * Created by Sergiusz on 20.07.2017.
+ */
+public class ImageManager {
+    private BufferedImage source;
+    private File sourceDirectory;
+    private File destinationDirectory;
+
+}
